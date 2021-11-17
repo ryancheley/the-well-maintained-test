b4b58396d055ea85addc178fd62992ff1aa8c46e
Ryan Cheley <rcheley@gmail.com>
2021-11-16 19:37:36 -0800

    updated version and added black and isort

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-VERSION = "0.1.3"
+VERSION = "0.2.0"
 
 
 def get_long_description():
@@ -33,7 +33,7 @@ setup(
     """,
     install_requires=["click", "requests", "rich"],
     extras_require={
-        "test": ["pytest"]
+        "test": ["pytest", "black", "isort"]
     },
     python_requires=">=3.7",
 )
