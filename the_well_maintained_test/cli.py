1786654c9f2cb8397ab41cd65f6974d5570bf97c
Ryan Cheley <rcheley@gmail.com>
2021-11-28 12:32:02 -0800

    updated output format

--- the_well_maintained_test/cli.py
+++ the_well_maintained_test/cli.py
@@ -26,6 +26,8 @@ from .utils import (
 console = Console()
 question_style = "bold blue"
 answer_style = ""
+answer_padding_style = (1, 0, 1, 4)
+special_answer_padding_style = (0, 0, 0, 4)
 
 try:  # pragma: no cover
     with open("auth.json") as f:
@@ -116,40 +118,40 @@ def url(url: str, branch: str, progress: bool) -> None:  # pragma: no cover
     tree_url = f"https://api.github.com/repos/{author}/{package}/git/trees/{default_branch}?recursive=1"
 
     console.print(questions.get("1"), style=question_style)
-    console.print(Padding(production_ready_check(pypi_url), (0, 4), style=answer_style))
+    console.print(Padding(production_ready_check(pypi_url), answer_padding_style, style=answer_style))
 
     console.print(questions.get("2"), style=question_style)
-    console.print(Padding(documentation_exists(pypi_url), (0, 4), style=answer_style))
+    console.print(Padding(documentation_exists(pypi_url), answer_padding_style, style=answer_style))
 
     console.print(questions.get("3"), style=question_style)
-    console.print(Padding(change_log_check(changelog, release), (0, 4), style=answer_style))
+    console.print(Padding(change_log_check(changelog, release), answer_padding_style, style=answer_style))
 
     console.print(questions.get("4"), style=question_style)
-    console.print(Padding(bug_responding(bugs_url, headers), (0, 4), style=answer_style))
+    console.print(Padding(bug_responding(bugs_url, headers), answer_padding_style, style=answer_style))
 
     console.print(questions.get("5"), style=question_style)
-    console.print(Padding(check_tests(tree_url, headers, progress), (0, 4), style=answer_style))
+    console.print(Padding(check_tests(tree_url, headers, progress), special_answer_padding_style, style=answer_style))
 
     console.print(questions.get("6"), style=question_style)
-    console.print(Padding(language_check(pypi_url), (0, 4), style=answer_style))
+    console.print(Padding(language_check(pypi_url), answer_padding_style, style=answer_style))
 
     console.print(questions.get("7"), style=question_style)
-    console.print(Padding(framework_check(pypi_url), (0, 4), style=answer_style))
+    console.print(Padding(framework_check(pypi_url), answer_padding_style, style=answer_style))
 
     console.print(questions.get("8"), style=question_style)
-    console.print(Padding(ci_setup(workflows_url, headers), (0, 4), style=answer_style))
+    console.print(Padding(ci_setup(workflows_url, headers), answer_padding_style, style=answer_style))
 
     console.print(questions.get("9"), style=question_style)
-    console.print(Padding(ci_passing(ci_status_url, headers), (0, 4), style=answer_style))
+    console.print(Padding(ci_passing(ci_status_url, headers), answer_padding_style, style=answer_style))
 
     console.print(questions.get("10"), style=question_style)
-    console.print(Padding(well_used(api_url, headers), (0, 4), style=answer_style))
+    console.print(Padding(well_used(api_url, headers), answer_padding_style, style=answer_style))
 
     console.print(questions.get("11"), style=question_style)
-    console.print(Padding(commit_in_last_year(commits_url, headers), (0, 4), style=answer_style))
+    console.print(Padding(commit_in_last_year(commits_url, headers), answer_padding_style, style=answer_style))
 
     console.print(questions.get("12"), style=question_style)
-    console.print(Padding(release_in_last_year(pypi_url), (0, 4), style=answer_style))
+    console.print(Padding(release_in_last_year(pypi_url), answer_padding_style, style=answer_style))
 
 
 @cli.command()
